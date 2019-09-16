e81fa5783293296977dad82e32bdc53b5c158707
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-17 00:02:52 +0200

    Added possibility to set coefficients

--- client.py
+++ client.py
@@ -1,20 +1,35 @@
 import requests
+import json
 
 class DynamicProcessSession():
 
     def __init__(self):
         self.__session = requests.Session()
         self.__address = 'http://20.0.0.2:8080/'
+        self.__dimension = 2
 
     def get_output(self):
         r = self.__session.get(self.__address + 'in_out/')
         print(r.text)
 
-    def set_input(self, data):
-        r = self.__session.put(self.__address + 'in_out/', data=data)
+    def set_input(self, value):
+        r = self.__session.put(self.__address + 'in_out/', data={'value':value})
+        print(r.text)
+
+    def get_coefficient(self, type):
+        r = self.__session.get(self.__address + 'coefficients/' + type + '/')
+        print(r.text)
+
+    def set_coefficient(self, type, value):
+        r = self.__session.put(self.__address + 'coefficients/' + type + '/',
+                               data={'value':value})
         print(r.text)
 
 
 dyn_process_session = DynamicProcessSession()
 dyn_process_session.get_output()
-dyn_process_session.set_input({'value':'1 2'})
+dyn_process_session.set_input('1 2')
+dyn_process_session.get_coefficient('A')
+dyn_process_session.set_coefficient('A', json.dumps([[1, 2],[2, 1]]))
+dyn_process_session.set_coefficient('GAMMA', json.dumps([[1, 1],[1, 1]]))
+dyn_process_session.set_coefficient('C', json.dumps([[1, 0],[0, 1]]))
--- server.py
+++ server.py
@@ -2,20 +2,21 @@ import random
 import string
 import cherrypy
 import numpy as np
-
+import json
 
 class DynamicProcess():
     def __init__(self):
         self.__y = np.array([0, 0])
         self.__x = np.array([0, 0])
-
-        self.__A = np.array([[1, 2],[2, 1]])
-        self.__GAMMA = np.array([[1, 1],[1, 1]])
-        self.__C = np.array([[1, 0],[0, 1]])
+        
+        self.coeff = {'A': np.array([[1, 2],[2, 1]]),
+                      'GAMMA': np.array([[1, 1],[1, 1]]),
+                      'C': np.array([[1, 0],[0, 1]])
+                      }
 
     def set_value(self, u):
-        self.__x = np.dot(self.__A, self.__x) + np.dot(self.__GAMMA, u)
-        self.__y = np.dot(self.__C, self.__x)
+        self.__x = np.dot(self.coeff['A'], self.__x) + np.dot(self.coeff['GAMMA'], u)
+        self.__y = np.dot(self.coeff['C'], self.__x)
 
     def get_value(self):
         return self.__y
@@ -50,6 +51,22 @@ class MeasureControlWebService(object):
         u = [float(i) for i in value.split()]
         self.__controller.set_value(u)
 
+@cherrypy.expose
+class CoefficientsWebService(object):
+
+    def __init__(self, dynamic_process):
+        self.__dynamic_process = dynamic_process
+
+    def GET(self, type):
+        coeff = self.__dynamic_process.coeff[type]
+        return np.array_str(coeff) 
+
+    def PUT(self, type, value):
+        value = json.loads(value)
+        value = np.array(value)
+
+        self.__dynamic_process.coeff[type] = value
+
 
 if __name__ == '__main__':
     dynamic_process = DynamicProcess()
@@ -62,6 +79,10 @@ if __name__ == '__main__':
         }
     }
     cherrypy.server.socket_host = '20.0.0.2'
-#    cherrypy.quickstart(MeasureControlWebService(), '/', conf)
-    cherrypy.quickstart(MeasureControlWebService(dynamic_process), '/in_out/',
+    cherrypy.tree.mount(MeasureControlWebService(dynamic_process), '/in_out/',
                         conf)
+    cherrypy.tree.mount(CoefficientsWebService(dynamic_process),
+                        '/coefficients/', conf)
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
