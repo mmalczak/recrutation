336e3f09647abbb5145ab85a651fa41171d93337
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-16 17:12:45 +0200

    Installed numpy

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,7 @@ apt-get install -y python3
 apt-get install -y python3-pip
 pip3 install cherrypy
 pip3 install requests
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+pip3 install numpy
