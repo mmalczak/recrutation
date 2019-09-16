29c32eaf93d3838c02adf7002746275eb4726d25
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-16 12:52:34 +0200

    Installed python3 with necessary packages for REST API

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get update
-#apt-get install -y vim
+apt-get install -y vim
 apt-get install -y python3
-
-
+apt-get install -y python3-pip
+pip3 install cherrypy
+pip3 install requests
