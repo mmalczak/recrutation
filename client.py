468a4fec006e6dc82795167c4f06c126c7a11531
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-17 14:04:05 +0200

    Receiving data from server as json, started adding a controller

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import time
+import numpy as np
+
 
 class DynamicProcessSession():
 
@@ -10,7 +13,8 @@ class DynamicProcessSession():
 
     def get_output(self):
         r = self.__session.get(self.__address + 'in_out/')
-        print(r.text)
+        return json.loads(r.text)
+        #print(r.text)
 
     def set_input(self, value):
         r = self.__session.put(self.__address + 'in_out/', data={'value':value})
@@ -34,9 +38,35 @@ class DynamicProcessSession():
                                data={'value':value})
         print(r.text)
 
+class Controller():
+    def __init__(self):
+        self.dimension = 2
+        self.__zero_init()
+
+    def __zero_init(self):
+        self.K = np.zeros([self.dimension, self.dimension]),
+
+    """OBSERVER"""
+    def get_est_state(self, y):
+        return y
+
+    """CONTROLLER"""
+    def get_control_signal(self, y):
+        x_est = self.get_est_state(y)
+        return np.dot(self.K, x_est)
+
+    def get_value(self):
+        return self.__y
+
+    def set_dimension(self, dimension):
+        self.dimension = dimension
+        self.__zero_init()
+
+
 
 
 dyn_process_session = DynamicProcessSession()
+controller = Controller()
 dyn_process_session.get_dimension()
 dyn_process_session.set_dimension('2')
 dyn_process_session.set_coefficient('A', json.dumps([[1, 2],[2, 1]]))
@@ -45,4 +75,12 @@ dyn_process_session.set_coefficient('GAMMA', json.dumps([[1, 1],[1, 1]]))
 dyn_process_session.set_coefficient('C', json.dumps([[1, 0],[0, 1]]))
 dyn_process_session.get_output()
 dyn_process_session.set_input('1 2')
+
+while(True):
+    y = dyn_process_session.get_output()
+    print(y)
+
+    #u = controller.get_control_signal(y)
+    dyn_process_session.set_input('1 2')
+    time.sleep(1)
 
