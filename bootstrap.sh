250437947c5686ceacef9df23b85769e3ada523f
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-18 21:26:49 +0200

    Updated python

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,16 @@
 
 apt-get update
 apt-get install -y vim
-apt-get install -y python3
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+apt-get install -y python3.6
 apt-get install -y python3-pip
-pip3 install cherrypy
-pip3 install requests
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-pip3 install numpy
+python3.6 -m pip install cherrypy
+python3.6 -m pip install requests 
+python3.6 -m pip install numpy 
+python3.6 -m pip install scipy 
+python3.6 -m pip install control 
+
