ca1490b7ed13d9141b7b522ff68f948cd246d0cd
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-22 13:01:49 +0200

    Issuing apt update only once

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y vim
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
 apt-get install -y python3.6
 apt-get install -y python3-pip
 export LANG="en_US.UTF-8"
