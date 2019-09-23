c91929c870dde0a9d59693607c54749119fde4a9
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-23 18:58:50 +0200

    Updated snort_conf and zaklocenie after moving from cherrypy to flask

--- zaklocenie.py
+++ zaklocenie.py
@@ -7,34 +7,32 @@ class DynamicProcessSession():
 
     def __init__(self):
         self.__session = requests.Session()
-        self.__address = 'http://20.0.0.2:8080/'
-
-    def set_nonexistent(self, data):
-        self.__session.put(self.__address + 'nonexistent/', 
-			   data={'value':data})
+        self.__address = 'http://20.0.0.2:5000/'
 
     def get_output(self):
-        r = self.__session.get(self.__address + 'in_out/')
-        return json.loads(r.text)
+        r = self.__session.get(self.__address + 'in_out')
+        data = r.json()
+        return json_to_np(data) 
 
     def set_input(self, value):
-        r = self.__session.put(self.__address + 'in_out/', data={'value':value})
+        r = self.__session.put(self.__address + 'in_out', data={'data':value})
 
     def get_coefficient(self, type):
-        r = self.__session.get(self.__address + 'coefficients/' + type + '/')
+        r = self.__session.get(self.__address + 'coefficients/' + type)
         print(r.text)
 
     def set_coefficient(self, type, value):
-        r = self.__session.put(self.__address + 'coefficients/' + type + '/',
-                               data={'value':value})
+        r = self.__session.put(self.__address + 'coefficients/' + type,
+                               data={'data':value})
 
     def get_num_states(self):
-        r = self.__session.get(self.__address + 'num_states/')
-        print(r.text)
+        r = self.__session.get(self.__address + 'num_states').json()
+        print(r)
 
     def set_num_states(self, value):
-        r = self.__session.put(self.__address + 'num_states/',
-                               data={'value':value})
+        r = self.__session.put(self.__address + 'num_states',
+                               data={'data': value})
+
 
 dyn_process_session = DynamicProcessSession()
 data = "11100010"
