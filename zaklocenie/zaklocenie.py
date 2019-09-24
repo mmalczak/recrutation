ccdf90cc00002427ad217751d1833a00ea10e2e2
Milosz Malczak <milosz.malczak@cern.ch>
2019-09-25 00:53:13 +0200

    Cleaned up zaklocenie

--- zaklocenie/zaklocenie.py
+++ zaklocenie/zaklocenie.py
@@ -9,29 +9,12 @@ class DynamicProcessSession():
         self.__session = requests.Session()
         self.__address = 'http://20.0.0.2:5000/'
 
-    def get_output(self):
-        r = self.__session.get(self.__address + 'in_out')
-        data = r.json()
-        return json_to_np(data) 
-
     def set_input(self, value):
         r = self.__session.put(self.__address + 'in_out', data={'data':value})
 
-    def get_coefficient(self, type):
-        r = self.__session.get(self.__address + 'coefficients/' + type)
-        print(r.text)
+    def set_nonexistent(self, value):
+        r = self.__session.put(self.__address + 'nonexistent', data={'data':value})
 
-    def set_coefficient(self, type, value):
-        r = self.__session.put(self.__address + 'coefficients/' + type,
-                               data={'data':value})
-
-    def get_num_states(self):
-        r = self.__session.get(self.__address + 'num_states').json()
-        print(r)
-
-    def set_num_states(self, value):
-        r = self.__session.put(self.__address + 'num_states',
-                               data={'data': value})
 
 
 dyn_process_session = DynamicProcessSession()
@@ -41,7 +24,7 @@ data = '[' + data + '], '
 last_data = '[' + data + ']'
 while(True):
 #    dyn_process_session.set_nonexistent(data)
-    dyn_process_session.set_input("[[1000000.03532324], [2]]") 
+    dyn_process_session.set_nonexistent("[[1000000.03532324], [2]]") 
 #    dyn_process_session.set_input("[[bleble.03532324], " + 
 #                                    data +
 #                                    data +
